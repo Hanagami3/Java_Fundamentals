15b719d71cafeb33574dde3c398e5d3bcd004591
Hanagami3 <145351834+Hanagami3@users.noreply.github.com>
2023-10-26 15:54:48 +0200

    Add files via upload

---
+++ Chapter08_Interfaces/src/be/brussel/les1/Project1/AudioPlayer/AudioPlayer.java
@@ -0,0 +1,11 @@
+package be.brussel.les1.Project1.AudioPlayer;
+
+public class AudioPlayer implements MediaPlayer{
+
+    MediaAdapter mediaAdapter = new MediaAdapter("");
+
+    @Override
+    public void play(String audioType, String fileName) {
+
+    }
+}
---
+++ Chapter08_Interfaces/src/be/brussel/les1/Project1/Customer/AbstractCustomer.java
@@ -0,0 +1,10 @@
+package be.brussel.les1.Project1.Customer;
+
+public abstract class AbstractCustomer {
+
+    protected static String name;
+
+    public abstract String getName();
+    public abstract boolean isNil();
+
+}
