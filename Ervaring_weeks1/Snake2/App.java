775cfe4364d42ac3dd9d22cd848f064457393c36
Hanagami3 <145351834+Hanagami3@users.noreply.github.com>
2023-11-02 02:06:48 +0100

    Add files via upload

---
+++ Ervaring_weeks1/Snake2/App.java
@@ -0,0 +1,26 @@
+package Snake2;
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception{
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+
+        JFrame frame = new JFrame("Snake");
+        frame.setSize(boardWidth,boardHeight);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
+
+
+    }
+}
